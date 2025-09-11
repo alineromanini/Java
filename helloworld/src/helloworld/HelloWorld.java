28764f1c367ed782b0224df3e1cfeb803ca4dd41
alineromanini <sromanini.aline@gmail.com>
2025-09-11 12:05:23 -0300

    Introdução ao Java

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {//public: qlq classe fora consegue inicializar o métdo;static: nao precisa de objeto; void nao retorna nada
+		System.out.println("Hello World!"); // ln pula linha....ctrl espaço pra aparecer opcoes de comandos
+		
+	}
+
+}
---
+++ helloworld/src/helloworld/Variaveis.java
@@ -0,0 +1,27 @@
+package helloworld;
+
+import java.util.Scanner;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in); //leia é o objeto da classe
+		
+		System.out.println("Digite o peso: ");
+		int peso = leia.nextInt();
+		
+		System.out.println("Digite o valor: ");
+		float valor = leia.nextFloat();
+		
+		System.out.println("Digite a opcao: ");
+		char opcao = leia.next().charAt(0);
+		
+		System.out.println("O valor da variável opcao é: " + opcao);
+		System.out.printf("O valor da variável valor é: %.2f \n", valor);
+		System.out.println("O valor da variável peso é: " + peso);
+
+		leia.close();
+	}
+
+}
